42b517cc195ea7cbd29344bf273fee0f073571fc
Luis Alberto Huaney Lino <tutec_corporation@tutec.pe>
2025-02-15 18:58:42 -0500

    correcion

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, { Suspense, lazy } from "react";
 import "./App.css";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router, // Cambiado a HashRouter
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
 
-
 import LoaderTerminalComponent from "./assets/components/LoaderTerminal";
 
 const SegmentsDocumentation = lazy(() => import('./assets/pages/ManualJsx'));
 const Error404 = lazy(() => import('./assets/pages/Errors/404'));
+
 const supportedLanguages = ["es", "en", "fr", "de", "zh"];
+
 const LanguageRedirect: React.FC = () => {
   let lang = navigator.language.slice(0, 2).toLowerCase();
   if (!supportedLanguages.includes(lang)) {
@@ -24,7 +25,7 @@ const LanguageRedirect: React.FC = () => {
 const App: React.FC = () => {
   return (
     <Router>
-      <Suspense fallback={<LoaderTerminalComponent/>}>
+      <Suspense fallback={<LoaderTerminalComponent />}>
         <Routes>
           <Route path="/" element={<LanguageRedirect />} />
           <Route path="/:lang/home" element={<SegmentsDocumentation />} />
